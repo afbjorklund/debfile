d750efe7d7e4984f4692b88fc1605b55e77aaa62
Anders F Björklund <anders.f.bjorklund@gmail.com>
2024-03-22 17:49:35 +0100

    Add old scripts for comparison

---
+++ dpkg.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+q="$1"
+test -n "$q" || exit 1
+
+pkg=$(dpkg -S "$q" 2>/dev/null | cut -d: -f1)
+# shellcheck disable=SC2016
+qf='${Package}_${Version}:${Architecture}\n'
+dpkg-query -f "$qf" -W "$pkg" 2>/dev/null || echo Unknown
