ab98c6128e750b554d9f2c49752254ec391ec728
Anders F Björklund <anders.f.bjorklund@gmail.com>
2024-06-03 12:00:44 +0200

    Remove space from dlocate status

--- dlocate.sh
+++ dlocate.sh
@@ -4,7 +4,7 @@ q="$1"
 test -n "$q" || exit 1
 
 pkg=$(dlocate -F "$q" | head -1 | cut -d: -f1)
-re='^..\s'$pkg'\s'
+re='^.. ?\s'$pkg'\s'
 COLUMNS=160 dlocate -P "$re" -l | tail -1 |
   awk '{ print $2"_"$3 }'
 test -n "$pkg" || exit 1
